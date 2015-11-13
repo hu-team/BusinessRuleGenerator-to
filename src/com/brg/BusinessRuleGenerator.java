06325ace74bedb9fc3cf9649da1bedde363003ec
Tom Valk <tomvalk@gmail.com>
2015-11-13 10:19:08 +0100

    Init git repo

---
+++ src/com/brg/BusinessRuleGenerator.java
@@ -0,0 +1,14 @@
+
+
+package com.brg;
+
+/**
+ * BusinessRuleGenerator main class
+ */
+public class BusinessRuleGenerator {
+
+    public static void main(String [] args) {
+        System.out.println("OK");
+    }
+
+}
