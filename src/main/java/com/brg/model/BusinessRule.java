4ccb8b7af1d2b5a07d46035c115b3feb3cdb2473
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-14 15:32:48 +0100

    Made the body for BusinessRule class.

--- src/main/java/com/brg/model/BusinessRule.java
+++ src/main/java/com/brg/model/BusinessRule.java
@@ -3,8 +3,67 @@ package com.brg.model;
 /**
  * Developed by Arjan.
  */
-public class BusinessRule {
+public abstract class BusinessRule {
 
+    private String code;
+    private String name;
+    private String description;
 
+    private Category category;
+    private RuleOperand operand;
+    private RuleValueBundle values;
 
+    public BusinessRule(RuleValueBundle bundle) {
+        this.values = bundle;
+    }
+
+    public abstract boolean validateRule();
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setOperand(RuleOperand operand) {
+        this.operand = operand;
+    }
+
+    public void setValues(RuleValueBundle values) {
+        this.values = values;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public RuleOperand getOperand() {
+        return operand;
+    }
+
+    public RuleValueBundle getValues() {
+        return values;
+    }
 }
