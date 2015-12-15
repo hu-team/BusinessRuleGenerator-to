5d06149931b3ffb4a79ae36891f8a6536840c1f1
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-15 13:58:14 +0100

    Made the body for Export class.

--- src/main/java/com/brg/generate/Export.java
+++ src/main/java/com/brg/generate/Export.java
@@ -1,7 +1,56 @@
 package com.brg.generate;
 
+import com.brg.domain.BusinessRule;
+import com.brg.domain.DatabaseType;
+import org.apache.ddlutils.model.Database;
+
+import java.util.ArrayList;
+
 /**
  * Developed by Arjan.
  */
 public class Export {
+
+    private String triggerIdentifier;
+    private String output;
+
+    private DatabaseType databaseType;
+
+    private ArrayList<ExportTemplate> templates;
+
+    public Export(BusinessRule rule){
+        templates = new ArrayList<ExportTemplate>();
+    }
+
+    public ArrayList<ExportTemplate> getTemplates() {
+        return this.templates;
+    }
+
+    public String getTriggerIdentifier() {
+        return this.triggerIdentifier;
+    }
+
+    public String getOutput() {
+        return this.output;
+    }
+
+    public DatabaseType getDatabaseType(){
+        return this.databaseType;
+    }
+
+    public void setDatabaseType(DatabaseType dt){
+        this.databaseType = dt;
+    }
+
+    public void setTriggerIdentifier(String triggerIdentifier) {
+        this.triggerIdentifier = triggerIdentifier;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+
+    public void setTemplates(ExportTemplate template) {
+        this.templates.add(template);
+    }
 }
