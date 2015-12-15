bc445f3fe06a0f5592d6f06e29dde1843d0cc882
Tom Valk <tomvalk@gmail.com>
2015-12-15 15:08:43 +0100

    Define tab, tab interface.

---
+++ src/main/java/com/brg/controller/DefineTab.java
@@ -0,0 +1,19 @@
+package com.brg.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DefineTab implements Initializable, TabControllerImpl {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void updateContents() {
+        System.out.println("Update tab!");
+    }
+}
