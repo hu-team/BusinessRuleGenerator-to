875559e37c7f7bb95c04c9059c396a31868a5262
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-15 13:45:29 +0100

    Made 3 export classes. MySQL Export and OracleExport implements the ExportTemplate interface.

---
+++ src/main/java/com/brg/generate/OracleExport.java
@@ -0,0 +1,35 @@
+package com.brg.generate;
+
+import com.brg.domain.DatabaseType;
+import com.brg.domain.RuleValueBundle;
+
+/**
+ * Developed by Arjan.
+ */
+public class OracleExport implements ExportTemplate {
+
+    @Override
+    public String fillTemplate(RuleValueBundle bundle) {
+        return null;
+    }
+
+    @Override
+    public DatabaseType getType() {
+        return null;
+    }
+
+    @Override
+    public String getCode() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getSource() {
+        return null;
+    }
+}
