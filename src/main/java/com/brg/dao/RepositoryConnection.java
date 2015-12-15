b1a9eeaf00e24f7174ac4eec7f6fe6899f62a970
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-15 15:09:53 +0100

    Made a getInstance method for the Connection classes.

--- src/main/java/com/brg/dao/RepositoryConnection.java
+++ src/main/java/com/brg/dao/RepositoryConnection.java
@@ -4,4 +4,15 @@ package com.brg.dao;
  * Developed by Arjan.
  */
 public class RepositoryConnection implements DatabaseConnection{
+
+    private RepositoryConnection instance;
+
+    public RepositoryConnection getInstance(){
+        if(instance == null) {
+            instance = new RepositoryConnection();
+        }
+
+        return instance;
+    }
+
 }
--- src/main/java/com/brg/dao/TargetConnection.java
+++ src/main/java/com/brg/dao/TargetConnection.java
@@ -5,5 +5,14 @@ package com.brg.dao;
  */
 public class TargetConnection implements DatabaseConnection {
 
+    private TargetConnection instance;
+
+    public TargetConnection getInstance(){
+        if(instance == null){
+            instance = new TargetConnection();
+        }
+
+        return instance;
+    }
 
 }
