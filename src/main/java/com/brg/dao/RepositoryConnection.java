61cbfb22c15ceeaa9ceb7e04312a3dacb9a52ff2
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-15 15:01:24 +0100

    Made the database connection interface with 2 connection classes that implements database connection.

---
+++ src/main/java/com/brg/dao/RepositoryConnection.java
@@ -0,0 +1,7 @@
+package com.brg.dao;
+
+/**
+ * Developed by Arjan.
+ */
+public class RepositoryConnection implements DatabaseConnection{
+}
---
+++ src/main/java/com/brg/dao/TargetConnection.java
@@ -0,0 +1,9 @@
+package com.brg.dao;
+
+/**
+ * Developed by Arjan.
+ */
+public class TargetConnection implements DatabaseConnection {
+
+
+}
