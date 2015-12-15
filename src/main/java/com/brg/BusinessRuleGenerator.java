73cbf9a911d72458b54cd04aa3355f2c108be2c0
Tom Valk <tomvalk@gmail.com>
2015-12-15 13:58:00 +0100

    Main will call the serviceprovider, etc to start the mainwindow.

--- src/main/java/com/brg/BusinessRuleGenerator.java
+++ src/main/java/com/brg/BusinessRuleGenerator.java
@@ -6,10 +6,12 @@ package com.brg;
  * BusinessRuleGenerator main class
  */
 public class BusinessRuleGenerator {
+    public static final String   VERSION = "1.0.0";
+    public static final int      BUILD = 1;
 
     public static void main(String [] args) {
-        System.out.println("OK");
-        System.out.println("PUSH IETS");
+        // Start the controller facade
+        ServiceProvider.getInstance().getControllerService().getMainWindow().start(args);
     }
 
 }
