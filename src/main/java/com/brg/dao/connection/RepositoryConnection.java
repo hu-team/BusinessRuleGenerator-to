05838f726cf49f9b06aaca746321918431210b3a
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-16 15:47:38 +0100

    Made the body for the connection class.

--- src/main/java/com/brg/dao/connection/RepositoryConnection.java
+++ src/main/java/com/brg/dao/connection/RepositoryConnection.java
@@ -1,11 +1,20 @@
 package com.brg.dao.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Developed by Arjan.
  */
-public class RepositoryConnection implements DatabaseConnection{
+public class RepositoryConnection {
 
     private RepositoryConnection instance;
+
+    private static String host;
+    private static String schema;
+    private static String username;
+    private static String password;
 
     public RepositoryConnection getInstance(){
         if(instance == null) {
@@ -13,6 +22,26 @@ public class RepositoryConnection implements DatabaseConnection{
         }
 
         return instance;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(host + schema, username, password);
+    }
+
+    public static void setHost(String h){
+        host = h;
+    }
+
+    public static void setSchema(String s){
+        schema = s;
+    }
+
+    public static void setUsername(String u){
+        username = u;
+    }
+
+    public static void setPassword(String p){
+        password = p;
     }
 
 }
--- src/main/java/com/brg/dao/connection/TargetConnection.java
+++ src/main/java/com/brg/dao/connection/TargetConnection.java
@@ -1,11 +1,20 @@
 package com.brg.dao.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Developed by Arjan.
  */
-public class TargetConnection implements DatabaseConnection {
+public class TargetConnection {
 
     private TargetConnection instance;
+
+    private static String host;
+    private static String schema;
+    private static String username;
+    private static String password;
 
     public TargetConnection getInstance(){
         if(instance == null){
@@ -13,6 +22,26 @@ public class TargetConnection implements DatabaseConnection {
         }
 
         return instance;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(host + schema, username, password);
+    }
+
+    public static void setHost(String h){
+        host = h;
+    }
+
+    public static void setSchema(String s){
+        schema = s;
+    }
+
+    public static void setUsername(String u){
+        username = u;
+    }
+
+    public static void setPassword(String p){
+        password = p;
     }
 
 }
