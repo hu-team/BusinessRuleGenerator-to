114c0cebda8b62dcb941d0a4e0c097bd3644e29a
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-16 15:47:38 +0100

    Made the body for the connection class.

--- src/main/java/com/brg/dao/connection/DatabaseConnection.java
+++ src/main/java/com/brg/dao/connection/DatabaseConnection.java
@@ -1,7 +1,15 @@
 package com.brg.dao.connection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Developed by Arjan.
  */
 public interface DatabaseConnection {
+
+    public Connection getConnection() throws SQLException;
+
+    //TODO: De static methods moeten nog in de interface komen maar ?????
+
 }
