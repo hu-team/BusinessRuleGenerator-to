170c1cdd68c8cfc0b261f4f036b8a3ef310e3365
Tom Valk <tomvalk@gmail.com>
2015-12-16 20:38:20 +0100

    Connection holder (only for prototype).

--- src/main/java/com/brg/persistence/PersistanceService.java
+++ src/main/java/com/brg/persistence/PersistanceService.java
@@ -2,13 +2,60 @@ package com.brg.persistence;
 
 
 import com.brg.common.AbstractFacadeService;
+import com.brg.common.Config;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class PersistanceService extends AbstractFacadeService implements PersistanceServiceImpl {
 
+    private Connection connection;
+
     private BusinessRuleService businessRuleService = new BusinessRuleService();
+
+    public PersistanceService() {
+
+        // TODO: Move this to the connection holder(s). This is only for prototype.
+        // TODO: Refactor
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException clfe) {
+            clfe.printStackTrace();
+            return;
+        }
+
+        Properties p;
+
+        try {
+            p = Config.getInstance().getConfig();
+        }catch (IOException ioe) {
+            ioe.printStackTrace();
+            return;
+        }
+
+        String connectString = "jdbc:oracle:thin:@" + p.getProperty("repository_host");
+        connectString += ":" + p.getProperty("repository_port");
+        connectString += "/" + p.getProperty("repository_service");
+
+        try{
+            connection = DriverManager.getConnection(connectString, p.getProperty("repository_username"), p.getProperty("repository_password"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 
     @Override
     public BusinessRuleService getBusinessRuleService() {
         return this.businessRuleService;
     }
+
+    @Override
+    public Connection getConnection() {
+        return this.connection;
+    }
+
 }
--- src/main/java/com/brg/persistence/PersistanceServiceImpl.java
+++ src/main/java/com/brg/persistence/PersistanceServiceImpl.java
@@ -1,5 +1,12 @@
 package com.brg.persistence;
 
+import java.sql.Connection;
+
 public interface PersistanceServiceImpl {
     BusinessRuleService getBusinessRuleService();
+
+
+    // Only for prototype
+    // TODO: refactor
+    Connection getConnection();
 }
