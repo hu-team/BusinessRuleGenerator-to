cd9c415c12a79391d60fee59d248f3b300501c6e
Tom Valk <tomvalk@gmail.com>
2016-01-07 20:56:25 +0100

    Adding docs.

--- src/main/java/com/brg/BusinessRuleGenerator.java
+++ src/main/java/com/brg/BusinessRuleGenerator.java
@@ -4,6 +4,7 @@ package com.brg;
 
 /**
  * BusinessRuleGenerator main class
+ * @author BRG Team 5
  */
 public class BusinessRuleGenerator {
     public static final String   VERSION = "1.0.0";
--- src/main/java/com/brg/analyse/AnalyseService.java
+++ src/main/java/com/brg/analyse/AnalyseService.java
@@ -3,6 +3,9 @@ package com.brg.analyse;
 
 import com.brg.common.AbstractFacadeService;
 
+/**
+ * Analyse Public Facade Service
+ */
 public class AnalyseService extends AbstractFacadeService implements AnalyseServiceImpl {
 
     private DatabaseService databaseService = new DatabaseService();
