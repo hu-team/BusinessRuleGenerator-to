c91d2d9180ec159f4366f5e70660e2550042f7b8
Tom Valk <tomvalk@gmail.com>
2016-01-12 12:58:21 +0100

    Update the way the bundle will be fetched.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.com/tomvlk/BusinessRuleGenerator-to.svg?token=A4bsEqeRNKNF2dXTCyxu&branch=prototype)](https://magnum.travis-ci.com/tomvlk/BusinessRuleGenerator-to) [![Codacy Badge](https://api.codacy.com/project/badge/grade/1baa08284b714bb69c18b2d15e079e70)](https://www.codacy.com)
+[![Build Status](https://travis-ci.com/tomvlk/BusinessRuleGenerator-to.svg?token=A4bsEqeRNKNF2dXTCyxu&branch=master)](https://travis-ci.com/tomvlk/BusinessRuleGenerator-to) [![Codacy Badge](https://api.codacy.com/project/badge/grade/1baa08284b714bb69c18b2d15e079e70)](https://www.codacy.com)
 =======
 # BusinessRuleGenerator-to
 Assessment HU. Business Rule Generator for PL/SQL
--- src/main/java/com/brg/dao/RuleValueBundleDAO.java
+++ src/main/java/com/brg/dao/RuleValueBundleDAO.java
@@ -28,24 +28,42 @@ public class RuleValueBundleDAO implements DAO{
                                 "WHERE bke.BundleID=" + set.getInt("BundleID") + " AND " +
                                 "ekv.EntryID = bke.EntryID" );
 
-                while(valueSet.next()) {
+                // Loop through the items in the bundle
+                while (valueSet.next()) {
                     String keyName = valueSet.getString("Key");
 
                     Integer intValue = valueSet.getInt("Int_Value");
-                    if(!valueSet.wasNull()){
+                    if (! valueSet.wasNull()){
                         bundle.putValue(keyName, intValue);
                     }
 
                     Float floatValue = valueSet.getFloat("Float_Value");
-                    if(!valueSet.wasNull()){
+                    if (! valueSet.wasNull()){
                         bundle.putValue(keyName, floatValue);
                     }
 
-                    if(valueSet.getString("String_Value") != null){
-                        bundle.putValue(keyName, valueSet.getString("String_Value"));
+                    if (valueSet.getString("String_Value") != null){
+                        if (bundle.getValue(keyName) != null && bundle.getValue(keyName) instanceof String) {
+                            // We are going to create an array inside!
+                            String currentString = "" + bundle.getValue(keyName); // Copy it first
+                            ArrayList<String> stringList = new ArrayList<String>();
+                            stringList.add(currentString);
+
+                            stringList.add(valueSet.getString("String_Value")); // Add new entry
+                        }
+
+                        if (bundle.getValue(keyName) != null && bundle.getValue(keyName) instanceof ArrayList) {
+                            ArrayList<String> stringList = (ArrayList<String>) bundle.getValue(keyName);
+                            stringList.add(valueSet.getString("String_Value")); // Add new entry
+                        }
+
+                        if (bundle.getValue(keyName) == null) {
+                            bundle.putValue(keyName, valueSet.getString("String_Value"));
+                        }
                     }
                 }
 
+                // Add bundle to the values
                 values.add(bundle);
 
                 valueSet.close();
