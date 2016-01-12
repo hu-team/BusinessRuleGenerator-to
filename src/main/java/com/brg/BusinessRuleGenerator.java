76f6cc19dd8266001a81c7b6bbfd13d4f7b935d2
Tom Valk <tomvalk@gmail.com>
2016-01-12 15:24:41 +0100

    Adding oracle db connector to gitignore for license agreement. Update build nr with current build.

--- src/main/java/com/brg/BusinessRuleGenerator.java
+++ src/main/java/com/brg/BusinessRuleGenerator.java
@@ -7,8 +7,8 @@ package com.brg;
  * @author BRG Team 5
  */
 public class BusinessRuleGenerator {
-    public static final String   VERSION = "1.0.0";
-    public static final int      BUILD = 1;
+    public static final String   VERSION    = "1.0.0";
+    public static final int      BUILD      = 160;
 
     public static void main(String [] args) {
         // Start the controller facade
