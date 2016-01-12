879761e4e84e86c1664650e95d4c949aadc3cb1f
Thierry Belliot <thierry.belliot@student.hu.nl>
2016-01-12 16:14:19 +0100

    get tables and columns

--- src/main/java/com/brg/analyse/DatabaseService.java
+++ src/main/java/com/brg/analyse/DatabaseService.java
@@ -1,12 +1,62 @@
 package com.brg.analyse;
 
 
+import com.brg.controller.TargetDatabase;
+import com.brg.dao.connection.TargetConnection;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ddlutils.Platform;
+import org.apache.ddlutils.PlatformFactory;
+import org.apache.ddlutils.model.Column;
+import org.apache.ddlutils.model.Database;
+import org.apache.ddlutils.model.Table;
+import org.apache.ddlutils.platform.JdbcModelReader;
+
+import javax.sql.DataSource;
+
 public class DatabaseService {
 
+    private TargetConnection targetConnection;
     /**
      * @see {http://db.apache.org/ddlutils/index.html} Reference to docs
      */
-    public DatabaseService() {
-
+    public DatabaseService() throws Exception {
+        targetConnection.getInstance();
     }
+
+    /**
+     * Get target database
+     * @return Database
+     * @throws Exception
+     */
+    public Database getDatabase() throws Exception {
+        DataSource dataSource = targetConnection.getDataSource();
+
+        Platform platform = PlatformFactory.createNewPlatformInstance(dataSource);
+        JdbcModelReader modelReader = platform.getModelReader();
+        Database database = modelReader.getDatabase(targetConnection.getConnection(), targetConnection.getSchema(), null, targetConnection.getSchema(), null);
+
+        return database;
+    }
+
+    /**
+     * Get target tables
+     * @return Table[]
+     * @throws Exception
+     */
+    public Table[] getTables() throws Exception {
+        Table[] tables = this.getDatabase().getTables();
+
+        return tables;
+    }
+
+    public Table getTable(int i) throws Exception {
+        Table table = this.getDatabase().getTable(i);
+        return table;
+    }
+
+    public Column[] getColumns(int tableId) throws Exception {
+        Column[] columns = this.getDatabase().getTable(tableId).getColumns();
+        return columns;
+    }
+
 }
