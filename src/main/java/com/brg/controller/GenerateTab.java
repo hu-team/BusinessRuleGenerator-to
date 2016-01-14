274693cb25a1b52c09a23d94141145a1f38d0f06
Olivier Hendriks <olivier.hendriks.limited@gmail.com>
2016-01-14 14:38:30 +0100

    Fixed null pointer exception. While exporting in the generate rule tab without selecting a rule it would still try to generate one, thus creating a null pointer.

--- src/main/java/com/brg/controller/GenerateTab.java
+++ src/main/java/com/brg/controller/GenerateTab.java
@@ -56,6 +56,10 @@ public class GenerateTab implements Initializable, TabControllerImpl {
 
     public void doExport(ActionEvent actionEvent) {
         String output = "Error!";
+        if(this.selectRule.getSelectionModel().getSelectedItem() == null ){
+            return;
+        }
+        
         try {
             output = ServiceProvider.getInstance().getExportService().createExport(this.selectRule.getSelectionModel().getSelectedItem()).getOutput();
         } catch (ClassNotFoundException e) {
