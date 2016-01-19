fa32409a96a8d4e4985116c1ae30d3f44c846f19
Thierry Belliot <thierry.belliot@student.hu.nl>
2016-01-19 14:01:23 +0100

    validaterulehelper added switch statement and methodes

--- src/main/java/com/brg/analyse/ValidateRuleHelper.java
+++ src/main/java/com/brg/analyse/ValidateRuleHelper.java
@@ -1,16 +1,62 @@
 package com.brg.analyse;
 
 import com.brg.ServiceProvider;
-import com.brg.analyse.DatabaseService;
+import com.brg.domain.RuleValueBundle;
 
 public class ValidateRuleHelper {
-    public void RuleType() {
+    private  DatabaseService databaseService;
+    private RuleValueBundle ruleValueBundle;
 
+    public ValidateRuleHelper(RuleValueBundle ruleValueBundle) {
+        databaseService = ServiceProvider.getInstance().getAnalyseService().getDatabaseService();
+        this.ruleValueBundle = ruleValueBundle;
+    }
+
+    /**
+    * Check which type must by validate
+     **/
+    public boolean RuleType(String type) {
+        boolean value = false;
+
+        switch (type) {
+            case "Attribute": value = AttributeValidate();
+                break;
+            case "Tuple": value = TupleValidate();
+                break;
+            case "Entity": value = EntityValidate();
+                break;
+        }
+
+        return value;
+    }
+
+    private boolean EntityValidate() {
+        return false;
+    }
+
+    private boolean TupleValidate() {
+        return false;
     }
 
     private boolean AttributeValidate() {
+        String table = "", column = "";
 
+        for(String key: ruleValueBundle.getKeys()) {
+            if(key.equals("attribute.column")) {
+
+                column = (String) ruleValueBundle.getValue(key);
+            }
+
+            if(key.equals("attribute.table")){
+                table = (String) ruleValueBundle.getValue(key);
+            }
+        }
+
+        if(databaseService.hasTable(table.toUpperCase()) && databaseService.hasColumn(table.toUpperCase(), column.toUpperCase())) {
+            return true;
+        }
 
         return false;
     }
+
 }
