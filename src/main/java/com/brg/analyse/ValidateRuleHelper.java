9013747d10698bb6a1b9c00714cbe88c9451e233
Thierry Belliot <thierry.belliot@student.hu.nl>
2016-01-20 12:11:19 +0100

    add inter entity validate

--- src/main/java/com/brg/analyse/ValidateRuleHelper.java
+++ src/main/java/com/brg/analyse/ValidateRuleHelper.java
@@ -25,17 +25,67 @@ public class ValidateRuleHelper {
                 break;
             case "Entity": value = EntityValidate();
                 break;
+            case "InterEntity": value = InterEntityValidate();
+                break;
         }
 
         return value;
     }
 
+    private boolean InterEntityValidate() {
+        String column1 = "", column2 = "", table1 = "", table2 = "";
+
+        for(String key: ruleValueBundle.getKeys()) {
+            if(key.equals("interentity.table.1")) {
+                table1 = (String) ruleValueBundle.getValue(key);
+            }
+
+            if(key.equals("interentity.table.2")) {
+                table2 = (String) ruleValueBundle.getValue(key);
+            }
+
+            if(key.equals("interentity.column.1")) {
+                column1 = (String) ruleValueBundle.getValue(key);
+            }
+
+            if(key.equals("interentity.column.2")) {
+                column2 = (String) ruleValueBundle.getValue(key);
+            }
+        }
+
+        if(databaseService.hasTable(table1.toUpperCase()) && databaseService.hasTable(table2.toUpperCase()) && databaseService.hasColumn(table1.toUpperCase(), column1.toUpperCase()) && databaseService.hasColumn(table2.toUpperCase(), column2.toUpperCase())) {
+            return true;
+        }
+
+        return false;
+    }
+
     private boolean EntityValidate() {
+        String table = "", column = "";
+
+        System.out.println(ruleValueBundle.getKeys());
+        for(String key: ruleValueBundle.getKeys()) {
+            if(key.equals("entity.table")) {
+                table = (String) ruleValueBundle.getValue(key);
+                System.out.println(table);
+            }
+
+            if(key.equals("entity.column")) {
+                column = (String) ruleValueBundle.getValue(key);
+                System.out.println(column);
+            }
+        }
+
+        if(databaseService.hasTable(table.toUpperCase()) && databaseService.hasColumn(table.toUpperCase(), column.toUpperCase())) {
+            return true;
+        }
+
         return false;
     }
 
     private boolean TupleValidate() {
         String table = "", column1 = "", column2 = "";
+
         for(String key: ruleValueBundle.getKeys()) {
             if(key.equals("tuple.table")) {
                 table = (String) ruleValueBundle.getValue(key);
