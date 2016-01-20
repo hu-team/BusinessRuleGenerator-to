a36de4910b2e38707ab6437e474946395b2869df
ArjanShoj <arjan.shojaeian@gmail.com>
2016-01-20 14:34:46 +0100

    Added some body to the about dialog.

--- src/main/java/com/brg/controller/MainWindow.java
+++ src/main/java/com/brg/controller/MainWindow.java
@@ -177,9 +177,29 @@ public class MainWindow extends Application implements Initializable {
         }else if(event.getSource() == aboutMenuItem){
 
             Alert about = new Alert(Alert.AlertType.INFORMATION);
-            about.setTitle("About us");
-            about.setHeaderText("About us");
-            String aboutContent = "";
+            about.setTitle("About");
+            about.setHeaderText("License");
+            String aboutContent = "The MIT License (MIT)\n" +
+                    "\n" +
+                    "Copyright (c) 2015-2016 Tom & HU Team 5\n" +
+                    "\n" +
+                    "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+                    "of this software and associated documentation files (the \"Software\"), to deal\n" +
+                    "in the Software without restriction, including without limitation the rights\n" +
+                    "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+                    "copies of the Software, and to permit persons to whom the Software is\n" +
+                    "furnished to do so, subject to the following conditions:\n" +
+                    "\n" +
+                    "The above copyright notice and this permission notice shall be included in all\n" +
+                    "copies or substantial portions of the Software.\n" +
+                    "\n" +
+                    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+                    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+                    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+                    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+                    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+                    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
+                    "SOFTWARE.\n";
             about.setContentText(aboutContent);
             about.show();
 
