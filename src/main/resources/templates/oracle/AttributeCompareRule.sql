bff1e13b4640cdc6294a3fbb4aec1118f2369bd7
Max van Kuik <kuikvanmax@hotmail.com>
2016-01-20 20:26:50 +0100

    ACMP Template fix

--- src/main/resources/templates/oracle/AttributeCompareRule.sql
+++ src/main/resources/templates/oracle/AttributeCompareRule.sql
@@ -3,25 +3,27 @@ BEFORE INSERT OR UPDATE
 ON {attribute_table}
 FOR EACH ROW
 DECLARE
-  L_PASSED      BOOLEAN := TRUE;
-  V_COLUMN_1    VARCHAR2(60) := :NEW.{attribute_column};
-  V_COMVALUE    VARCHAR2(255) := '{compare_with}';
+  L_PASSED       BOOLEAN := FALSE;
+  V_COLUMN_1     VARCHAR2(60) := :NEW.{attribute_column};
+  V_COMVALUE     VARCHAR2(255) := '{compare_with}';
   V_COMVALUE_NUM NUMBER(10);
-  BEGIN
+BEGIN
 
-    V_COMVALUE_NUM := TO_NUMBER(V_COMVALUE);
+
+  IF REGEXP_LIKE(V_COMVALUE, '^[[:digit:]]+$') THEN
+  V_COMVALUE_NUM := TO_NUMBER(V_COMVALUE);
 
     IF (V_COLUMN_1 {operand} V_COMVALUE_NUM) THEN
       L_PASSED := TRUE;
     ELSE
       RAISE_APPLICATION_ERROR(-20000, 'Error Raised: ' || {error});
     END IF;
-
-EXCEPTION
-  WHEN VALUE_ERROR THEN
+  END IF;
+  IF NOT L_PASSED THEN
     IF (V_COLUMN_1 {operand} V_COMVALUE) THEN
       L_PASSED := TRUE;
     ELSE
       RAISE_APPLICATION_ERROR(-20000, 'Error Raised: ' || {error});
     END IF;
-  END;
+  END IF;
+END;
