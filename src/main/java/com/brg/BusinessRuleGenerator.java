57dc609912991913197b4c027d45bccc08fce81c
Tom Valk <tomvalk@gmail.com>
2016-01-21 09:42:25 +0100

    Release 1.0.0

--- src/main/java/com/brg/BusinessRuleGenerator.java
+++ src/main/java/com/brg/BusinessRuleGenerator.java
@@ -7,8 +7,8 @@ package com.brg;
  * @author BRG Team 5
  */
 public class BusinessRuleGenerator {
-    public static final String   VERSION    = "1.0.0-beta1";
-    public static final int      BUILD      = 283;
+    public static final String   VERSION    = "1.0.0";
+    public static final int      BUILD      = 285;
     public static final boolean  DEBUG      = true;
 
     public static void main(String [] args) {
